73760df731e549dd7782398cf2d5b7e0ad4b562b
Thomas Darkson <152335332+ThomasDarkson@users.noreply.github.com>
2025-02-09 21:26:35 +0300

    1.21.4

---
+++ src/main/java/reworked/ancient/debris/ReworkedAncientDebris.java
@@ -0,0 +1,11 @@
+package reworked.ancient.debris;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ReworkedAncientDebris implements ModInitializer {
+	public static final String MOD_ID = "reworked-ancient-debris";
+
+	@Override
+	public void onInitialize() {
+	}
+}
